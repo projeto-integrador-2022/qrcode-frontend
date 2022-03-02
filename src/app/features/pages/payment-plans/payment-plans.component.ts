9cd3241b271e8339d947b9c156a7b3eaa20eb180
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-03-02 17:31:57 -0300

    [QRCT-001] Pagina 'planos de pagamentos' modelada e funcional conforme prototipo

--- src/app/features/pages/payment-plans/payment-plans.component.ts
+++ src/app/features/pages/payment-plans/payment-plans.component.ts
@@ -33,8 +33,37 @@ export class PaymentPlansComponent implements OnInit {
     },
   ];
 
-  size: number = this.cardsContents.length;
+  readonly faqQuestions = [
+    { 
+      title: 'Quais cartões de créditos aceitos?',
+      description: 'Nullam mauris nulla, placerat egestas elit et, venenatis porttitor nisl. Nulla vel velit eu lectus bibendum blandit eu molestie nisl. Mauris sed cursus sem. '
+    },
+    { 
+      title: 'Posso pagar por Pix?', 
+      description: 'Vivamus venenatis erat ullamcorper auctor congue. Nunc varius, odio id maximus congue, velit diam elementum dolor, at eleifend est dui non nisl.'
+    },
+    { 
+      title: 'Posso cancelar a qualquer momento?', 
+      description: 'Sed neque tellus, finibus sed rhoncus a, vehicula eu enim. Cras condimentum lorem vel nisl interdum, ac placerat augue semper. Quisque iaculis dictum ex.'
+    },
+    { 
+      title: 'Posso dividir minha assinatura?', 
+      description: 'Nunc a pretium massa. Donec nulla turpis, rutrum eget metus ultricies, eleifend sagittis diam. Donec fringilla erat enim. Integer luctus nisl tortor, at lacinia.'
+    },
+    { 
+      title: 'Como são entregues os relatórios mensais?', 
+      description: 'lectus cursus tincidunt. Nullam lobortis ornare commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    },
+    { 
+      title: 'O que são relatórios em tempo real?', 
+      description: 'Quisque suscipit aliquet egestas. Etiam euismod placerat quam et rhoncus. Nunc mollis dolor at mi gravida ornare. Sed hendrerit egestas ultrices.'
+    }
+
+  ]
+  cardContentSize: number = this.cardsContents.length;
+  faqQuestionsSize: number = this.faqQuestions.length;
   
+
   ngOnInit(): void {
   }
   navigateTo() {
