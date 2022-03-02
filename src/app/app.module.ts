3c93960e978ff587bdd10c020d70a03b922826b5
GUSTAVO HENRIQUE DE SOUZA RORIZ <50633357+ghsroriz@users.noreply.github.com>
2022-03-02 19:17:10 -0300

    Revert "Feature/qrct 001"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './features/pages/landing-page/landing-page.component';
-import { NavBarComponent } from './features/components/nav-bar/nav-bar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AboutComponent } from './features/pages/about/about.component';
-import { UsComponent } from './features/pages/us/us.component';
-import { LoginComponent } from './features/pages/login/login.component';
-import { FooterComponent } from './features/components/footer/footer.component';
-import { PaymentPlansComponent } from './features/pages/payment-plans/payment-plans.component';
-import { ContactUsFormComponent } from './features/components/contact-us-form/contact-us-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingPageComponent,
-    NavBarComponent,
-    AboutComponent,
-    UsComponent,
-    LoginComponent,
-    FooterComponent,
-    PaymentPlansComponent,
-    ContactUsFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
