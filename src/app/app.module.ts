e8a1dff152d754f4ad2ba0f1d6bf6c01cffc8b0b
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-03-02 13:35:04 -0300

    [QRCT-001] Pagina de 'sobre' implementada com novo componente de 'formulario de contato'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UsComponent } from './features/pages/us/us.component';
 import { LoginComponent } from './features/pages/login/login.component';
 import { FooterComponent } from './features/components/footer/footer.component';
 import { PaymentPlansComponent } from './features/pages/payment-plans/payment-plans.component';
+import { ContactUsFormComponent } from './features/components/contact-us-form/contact-us-form.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { PaymentPlansComponent } from './features/pages/payment-plans/payment-pl
     UsComponent,
     LoginComponent,
     FooterComponent,
-    PaymentPlansComponent
+    PaymentPlansComponent,
+    ContactUsFormComponent
   ],
   imports: [
     BrowserModule,
