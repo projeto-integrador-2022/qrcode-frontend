3e572874502ca74ea5f349563b4ccb27b816f6eb
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-03-04 19:31:28 -0300

    [QRCT-001] Todas as paginas ate agora modeladas estao puxando dados do arquivo sentences.json em /assets/lib

--- src/app/features/pages/us/us.component.ts
+++ src/app/features/pages/us/us.component.ts
@@ -1,38 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import SENTENCES from '../../../../assets/lib/sentences.json';
 
 @Component({
   selector: 'app-us',
   templateUrl: './us.component.html',
-  styleUrls: ['./us.component.scss']
+  styleUrls: ['./us.component.scss'],
 })
 export class UsComponent implements OnInit {
-  imageUrl: string = '../../../assets/images/banner-example.png'
-  constructor() { }
-
-  readonly jumbotronContent = [
-    { 
-      title: 'A Big Hiccups Soluções',
-      description1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec blandit tortor et consectetur.',
-      description2: 'Phasellus ullamcorper porttitor eleifend. In in ante quis libero convallis tempor nec quis arcu.',
-      description3: 'Pellentesque tristique ipsum a venenatis auctor. Duis metus turpis, fermentum eget  orci  vitae.',
-    },
-    { 
-      title: 'Nossa Equipe',
-      description1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec blandit tortor et consectetur.',
-      description2: 'Phasellus ullamcorper porttitor eleifend. In in ante quis libero convallis tempor nec quis arcu.',
-      description3: 'Pellentesque tristique ipsum a venenatis auctor. Duis metus turpis, fermentum eget  orci  vitae.',
-    },
-    { 
-      title: 'Fale Conosco!',
-      description1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec blandit tortor et consectetur.',
-      description2: 'Phasellus ullamcorper porttitor eleifend. In in ante quis libero convallis tempor nec quis arcu.',
-      description3: 'Pellentesque tristique ipsum a venenatis auctor. Duis metus turpis, fermentum eget  orci  vitae.',
-    },
-  ];
-
-  size: number = this.jumbotronContent.length;
-
-  ngOnInit(): void {
+  imageUrl: string = '../../../assets/images/banner-example.png';
+  US_SENTENCES: any;
+  size: number = 0;
+  constructor() {
+    this.US_SENTENCES = SENTENCES.US;
   }
 
+  ngOnInit(): void {
+    this.size = this.US_SENTENCES.length;
+  }
 }
