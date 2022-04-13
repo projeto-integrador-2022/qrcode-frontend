9b062fbf5b082f6916ca9d1b2a8388a1a180c177
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-04-13 17:34:13 -0300

    [QRCT-003F] Início de implementação de páginas internas

--- src/app/features/pages/admin-page/admin-page.component.ts
+++ src/app/features/pages/admin-page/admin-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { AuthGuard } from 'src/app/shared/services/auth.guard';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
-  styleUrls: ['./admin-page.component.scss']
+  styleUrls: ['./admin-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AdminPageComponent implements OnInit {
 
---
+++ src/app/features/pages/admin-page/starting/starting.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import SENTENCES from '../../../../../assets/lib/sentences.json'
+
+@Component({
+  selector: 'starting-component',
+  templateUrl: './starting.component.html',
+  styleUrls: ['./starting.component.scss']
+})
+export class StartingComponent implements OnInit {
+  STARTING_SENTENCES: any;
+
+  constructor() {
+    this.STARTING_SENTENCES = SENTENCES.STARTING;
+  }
+
+  ngOnInit(): void {
+    console.log(this.STARTING_SENTENCES);
+    
+  }
+
+}
