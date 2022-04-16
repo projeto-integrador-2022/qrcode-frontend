874631b18834ec37812ff09e53bb955c92afe97b
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-04-16 12:13:18 -0300

    [QRCT-003F] Componentes internos agora fazem parte apenas da rota pai 'admin-page'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,18 +8,32 @@ import { PaymentMethodsComponent } from './features/pages/payment-methods/paymen
 import { UsComponent } from './features/pages/us/us.component';
 import { AdminPageComponent } from './features/pages/admin-page/admin-page.component';
 import { AuthGuard } from './shared/services/auth.guard';
+import { StartingComponent } from './features/pages/admin-page/starting/starting.component';
 
 const routes: Routes = [
-  
-    { path:'', component:LandingPageComponent },
-    { path:'landing-page', component:LandingPageComponent },
-    { path:'about', component:AboutComponent },
-    { path:'us', component:UsComponent },
-    { path:'login', component:LoginComponent, data: { username: '', password: '' } },
-    { path:'payment-plans', component:PaymentPlansComponent },
-    { path:'payment-methods', component:PaymentMethodsComponent, data: { index:''} },
-    { path:'admin-page', component:AdminPageComponent, canActivate:[AuthGuard], data: { username:''} },
-  ];
+
+  { path: '', component: LandingPageComponent },
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'us', component: UsComponent },
+  { path: 'login', component: LoginComponent, data: { username: '', password: '' } },
+  { path: 'payment-plans', component: PaymentPlansComponent },
+  { path: 'payment-methods', component: PaymentMethodsComponent, data: { index: '' } },
+  {
+    path: 'admin-page', component: AdminPageComponent, canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'starting',
+        component: StartingComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'starting',
+      },
+
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
