3bacca0649133e372977a8815ea9b263b368a6cd
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-04-18 17:01:24 -0300

    [QRCT-004F] Tela de gerador de código QR-continuação

--- src/app/features/pages/admin-page/qr-generator/qr-generator.component.ts
+++ src/app/features/pages/admin-page/qr-generator/qr-generator.component.ts
@@ -9,11 +9,35 @@ import SENTENCES from '../../../../../assets/lib/sentences.json'
   encapsulation: ViewEncapsulation.None
 })
 export class QrGeneratorComponent implements OnInit {
-  GENERATOR_SENTENCES: any;
+
   formGroup: any;
   selected = '';
+  formData: any;
+  GENERATOR_SENTENCES: any;
+  IMAGE: any;
+  ADMIN_SENTENCES: any;
+
   constructor(private formBuilder: FormBuilder) { 
     this.GENERATOR_SENTENCES = SENTENCES.GENERATOR;
+    this.IMAGE = SENTENCES.MOBILE;
+    this.ADMIN_SENTENCES = SENTENCES.ADMIN_PAGE;
+
+    this.formData = [
+      {
+        name: '',
+        cnpj: '',
+        email: '',
+        officialpage: '',
+        productpage: '',
+        whatsappgroup: '',
+        telegramgroup: '',
+        facebookgroup: '',
+        instagramgroup: '',
+        voucherpage: '',
+        banner: '',
+        announcement: ''
+      }
+    ]
   }
 
   ngOnInit(): void {
@@ -22,12 +46,34 @@ export class QrGeneratorComponent implements OnInit {
   }
 
   createForm() {
+    let emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    let cnpj: RegExp = /(\d{2}[-.\s]?\d{3}[-.\s]?\d{3}[-.\s\/]?\d{4}[-.\s]?\d{2})$/
+
     this.formGroup = this.formBuilder.group({
       'name' : [null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      'cnpj' : [null, Validators.pattern(cnpj)],
+      'email' : [null, [Validators.required, Validators.pattern(emailregex)]],
+      'officialpage' : [null],
+      'productpage' : [null],
+      'whatsappgroup': [null],
+      'telegramgroup': [null],
+      'facebookgroup': [null],
+      'instagramgroup': [null],
+      'voucherpage': [null],
+      'banner': [null],
+      'announcement': [null],
     });
   }
 
-  onSubmit(value: any) {
+  updateMobileView(event: any, formName: string) {
+    console.log('🚀 >> event: any, formName: string', event.target.value, formName);
+    this.formData[0][formName] = event.target.value;
+    console.log('🚀 >> this.formData', this.formData[0][formName]);
+    
+    
+  }
+
+  onSubmit(formData: any) {
   }
 
 
