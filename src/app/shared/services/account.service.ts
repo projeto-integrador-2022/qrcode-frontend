6e154e3a153986f2b36f0bb56bc864d4f6aabffa
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-06-10 17:39:03 -0300

    Ajustments

--- src/app/shared/services/account.service.ts
+++ src/app/shared/services/account.service.ts
@@ -10,8 +10,8 @@ import { Account } from '../models/account';
 })
 export class AccountService {
 
-  endpoint = 'http://20.226.8.18/authenticate';
-  //endpoint = 'http://localhost:8080/administrator';
+  authEndpoint = 'http://20.226.8.18/authenticate';
+  accountEndpoint = 'http://20.226.8.18/administrator';
 
   constructor(private http: HttpClient) { }
 
@@ -24,7 +24,7 @@ export class AccountService {
   
   createAccount(account : Account): Observable<Account> {
     return this.http
-      .post<Account>(this.endpoint, JSON.stringify(account), this.httpOptions)
+      .post<Account>(this.accountEndpoint, JSON.stringify(account), this.httpOptions)
       .pipe(
         retry(2),
         catchError(this.handleError)
@@ -33,49 +33,12 @@ export class AccountService {
 
   login(loginParams : any): Observable<Account> {
     return this.http
-      .post<Account>(this.endpoint, JSON.stringify(loginParams), this.httpOptions)
+      .post<Account>(this.authEndpoint, JSON.stringify(loginParams), this.httpOptions)
       .pipe(
         retry(2),
         catchError(this.handleError)
       )
   }
-
-
-  // getAccounts(): Observable<Account> {
-  //   return this.http
-  //     .get<Account>(this.endpoint)
-  //     .pipe(
-  //       retry(2),
-  //       catchError(this.handleError)
-  //     )
-  // }
-
-  // getItem(id: number): Observable<Account> {
-  //   return this.http
-  //     .get<Account>(this.endpoint + '/' + id)
-  //     .pipe(
-  //       retry(2),
-  //       catchError(this.handleError)
-  //     )
-  // }
-
-  // updateItem(id: number, item: Account): Observable<Account> {
-  //   return this.http
-  //     .put<Account>(this.endpoint + '/' + id, JSON.stringify(item), this.httpOptions)
-  //     .pipe(
-  //       retry(2),
-  //       catchError(this.handleError)
-  //     )
-  // }
-
-  // deleteItem(id: number) {
-  //   return this.http
-  //     .delete<Account>(this.endpoint + '/' + id, this.httpOptions)
-  //     .pipe(
-  //       retry(2),
-  //       catchError(this.handleError)
-  //     )
-  // }
 
   handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
