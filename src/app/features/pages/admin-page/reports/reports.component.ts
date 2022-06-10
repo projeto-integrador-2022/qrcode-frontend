d0e27cbd146c94d1e0d436031c4e413f4548c5be
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-06-10 18:33:12 -0300

    [QRCT-006F] Mocked table implementation

--- src/app/features/pages/admin-page/reports/reports.component.ts
+++ src/app/features/pages/admin-page/reports/reports.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss']
+  styleUrls: ['./reports.component.scss'],
 })
 export class ReportsComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['position', 'storeName', 'name', 'email'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   ngOnInit(): void {
+    
   }
-
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  storeName: string;
+  email: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    position: 1, 
+    storeName: 'Cine Astro', 
+    name: 'John Doe', 
+    email: 'john.doe@gmail.com'
+  },
+
+
+];
+
