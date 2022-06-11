9bd733571ec2f53b2fd5754eee30a78cfae2d68d
ghsroriz <gustavo-hsr1994@hotmail.com>
2022-06-10 23:09:19 -0300

    https

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,6 +5,7 @@ export const environment = {
         nav_bar: "../../../assets/images/qr-code-color.png",
         about: '../../../assets/images/qr-code-color.png',
         us:'../../../assets/images/ppl-working-cropped.jpg',
-    }
+    },
+    baseUrl: 'http://20.226.8.18'
   };
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,8 @@ export const environment = {
     nav_bar: "../../../assets/images/qr-code-color.png",
     about: '../../../assets/images/qr-code-color.png',
     us:'../../../assets/images/ppl-working-cropped.jpg',
-  }
+  },
+  baseUrl: 'http://20.226.8.18'
 };
 
 /*
